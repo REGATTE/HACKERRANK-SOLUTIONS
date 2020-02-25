59f6f440a4eeaf852ac7a7a0fdb92ae34df11ad8
Ashok <ashok_a380@icloud.com>
2020-02-25 15:09:55 +0530

    git commit

---
+++ q2.py
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2511
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;\red13\green16\blue23;\red240\green245\blue245;}
+{\*\expandedcolortbl;;\cssrgb\c5490\c7843\c11765;\cssrgb\c95294\c96863\c96863;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+print("Hello, World!")}
---
+++ q5.py
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2511
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red255\green255\blue254;\red0\green0\blue255;
+}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c100000\c100000\c99608;\cssrgb\c0\c0\c100000;
+}
+\paperw11900\paperh16840\margl1440\margr1440\vieww17720\viewh10240\viewkind0
+\deftab720
+\pard\pardeftab720\sl460\partightenfactor0
+
+\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 a\'a0=\'a0\cf4 \strokec4 int\cf2 \strokec2 (\cf4 \strokec4 input\cf2 \strokec2 ())\cb1 \
+\cb3 b\'a0=\'a0\cf4 \strokec4 int\cf2 \strokec2 (\cf4 \strokec4 input\cf2 \strokec2 ())\cb1 \
+\
+\pard\pardeftab720\sl460\partightenfactor0
+\cf4 \cb3 \strokec4 print\cf2 \strokec2 (a//b)\cb1 \
+\cf4 \cb3 \strokec4 print\cf2 \strokec2 (a/b)\cb1 \
+}
